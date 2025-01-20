959460fa9d3ad993bcadf1934433a100ab5e6695
Henri_Mello <henri.franca@hotmail.com>
2025-01-19 23:07:15 -0300

    feat: create a simple bot code

---
+++ simple_bot/main.py
@@ -0,0 +1,8 @@
+name = input("Hello! What is your name? ")
+print(f"Nice to meet you, {name}!")
+age_input = int(input("How old are you? "))
+bot_age = 3
+age_difference = age_input - bot_age
+print(f"You're {age_difference} years old than me. I'm only {bot_age} years old!")
+color = input("What's your favorite color? ")
+print(f"Oh, {color} is a beautiful color!")
